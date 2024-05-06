912688f66832b32ca4aaf3af376fabb431b9a0a1
MDorizon <mdorizon@proton.me>
2024-05-06 22:05:39 +0200

    :bug: some bug resolved on ready event

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 const { Client, GatewayIntentBits } = require('discord.js');
 
-const eventHandler = require('');
+const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
     intents: [
