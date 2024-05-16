04197f6b5fc2be7803f88c6bf81cffbd41cc8905
MDorizon <mdorizon@proton.me>
2024-05-16 11:46:27 +0200

    :sparkles: add connection to database

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,5 +1,13 @@
 require("colors");
+const mongoose = require('mongoose');
+const mongoURL = process.env.MONGO_URL;
 
-module.exports = (client) => {
+module.exports = async (client) => {
     console.log(`[INFO] ${client.user.username} est en ligne !`.bgBlue);
+
+    if (!mongoURL) return;
+    mongoose.set('strictQuery', true);
+    if (await mongoose.connect(mongoURL)) {
+        console.log(`[INFO] Connected to the database`.green);
+    }
 }
